46f15b5f9f1806d1c8a052553bdd5021483e719b
MakeNuclearWithRice <rlatjdqls34@gmail.com>
2024-11-25 22:26:36 +0900

    first commit

---
+++ src/main/java/com/example/traffic_coupon/TrafficCouponApplication.java
@@ -0,0 +1,13 @@
+package com.example.traffic_coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficCouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficCouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/traffic_coupon/TrafficCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.traffic_coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
